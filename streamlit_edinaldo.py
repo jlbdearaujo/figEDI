d90888b06f084a7d111ff2586be3c24ec861490a
jlbdearaujo <76960883+jlbdearaujo@users.noreply.github.com>
2021-07-15 22:13:52 -0300

    Update streamlit_edinaldo.py

--- streamlit_edinaldo.py
+++ streamlit_edinaldo.py
@@ -422,21 +422,21 @@ if paginaseleciona=='FIG6':
 
     #plt.rcParams.update({'font.size': 40})
 
-    c1aux=EDIALL[EDIALL['CLUSTER']==1].copy()
-    data_to_plot=[c1aux[c1aux['α']==0.5]['expP'].values,c1aux[c1aux['α']==0.55]['expP'].values,c1aux[c1aux['α']==0.6]['expP'].values,c1aux[c1aux['α']==0.65]['expP'].values]
+    #c1aux=EDIALL[EDIALL['CLUSTER']==1].copy()
+    #data_to_plot=[c1aux[c1aux['α']==0.5]['expP'].values,c1aux[c1aux['α']==0.55]['expP'].values,c1aux[c1aux['α']==0.6]['expP'].values,c1aux[c1aux['α']==0.65]['expP'].values]
    
     fig,ax=plt.subplots(figsize=(10,7))
     x1 = [1,2,3,4]
     squad = [0.50,0.55,0.60,0.65]
     plt.ylabel(r'$\beta$',fontsize=40)
-    plt.xticks(x1,squad,fontsize=40)
+    #plt.xticks(x1,squad,fontsize=40)
     plt.xlabel(r'$\alpha$',fontsize=40)
     ax.xaxis.set_tick_params(width=3)
     ax.yaxis.set_tick_params(width=3)
     for axis in ['top','bottom','left','right']:
         ax.spines[axis].set_linewidth(3)
     #bp=plt.boxplot(data_to_plot,labels=squad)
-    plt.plot([1,2,3],[10,20,30])
+    plt.plot([1,2,3,4],[10,20,30])
     st.pyplot(fig)
 
     
