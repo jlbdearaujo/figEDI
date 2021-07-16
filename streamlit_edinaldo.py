57656bcb7695cc38a80fa6eab9d296ec19285a00
jlbdearaujo <76960883+jlbdearaujo@users.noreply.github.com>
2021-07-15 22:12:35 -0300

    Update streamlit_edinaldo.py

--- streamlit_edinaldo.py
+++ streamlit_edinaldo.py
@@ -420,7 +420,7 @@ if paginaseleciona=='FIG6':
     st.write('Espere um pouco, a figura pode demorar a renderizar')
 
 
-    plt.rcParams.update({'font.size': 40})
+    #plt.rcParams.update({'font.size': 40})
 
     c1aux=EDIALL[EDIALL['CLUSTER']==1].copy()
     data_to_plot=[c1aux[c1aux['α']==0.5]['expP'].values,c1aux[c1aux['α']==0.55]['expP'].values,c1aux[c1aux['α']==0.6]['expP'].values,c1aux[c1aux['α']==0.65]['expP'].values]
@@ -428,9 +428,9 @@ if paginaseleciona=='FIG6':
     fig,ax=plt.subplots(figsize=(10,7))
     x1 = [1,2,3,4]
     squad = [0.50,0.55,0.60,0.65]
-    plt.ylabel(r'$$\beta$$',fontsize=40)
+    plt.ylabel(r'$\beta$',fontsize=40)
     plt.xticks(x1,squad,fontsize=40)
-    plt.xlabel(r'$$\alpha$$',fontsize=40)
+    plt.xlabel(r'$\alpha$',fontsize=40)
     ax.xaxis.set_tick_params(width=3)
     ax.yaxis.set_tick_params(width=3)
     for axis in ['top','bottom','left','right']:
